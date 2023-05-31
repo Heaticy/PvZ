2b548babbfd847fa861d0c6d695efe239e1934d3
heaticy <1712371381@qq.com>
2023-05-31 13:12:28 +0800

    1

--- src/GameObject/GameObject.hpp
+++ src/GameObject/GameObject.hpp
@@ -3,6 +3,23 @@
 
 #include <memory>
 #include "ObjectBase.hpp"
+#include <random>
+
+class Random
+{
+private:
+  std::random_device rd;
+  std::mt19937 generator;
+  std::uniform_int_distribution<int> distribution;
+
+public:
+  Random() : generator(rd()), distribution(63, 263) {}
+
+  int generateRandomNumber()
+  {
+    return distribution(generator);
+  }
+};
 
 // Declares the class name GameWorld so that its pointers can be used.
 class GameWorld;
@@ -14,8 +31,18 @@ public:
   GameObject(ImageID imageID, int x, int y, LayerID layer, int width, int height, AnimID animID) : ObjectBase(imageID, x, y, layer, width, height, animID)
   {
   }
+  GameObject(ImageID imageID, int x, int y, LayerID layer, int width, int height, AnimID animID, int hp) : ObjectBase(imageID, x, y, layer, width, height, animID), m_hp(hp)
+  {
+  }
+  void SetHp(int hp)
+  {
+    m_hp = hp;
+  }
 
-private:
+  virtual int isZombie() = 0;
+
+protected:
+  int m_hp = 1;
 };
 
 using pGameObject = std::shared_ptr<GameObject>;
@@ -26,5 +53,68 @@ public:
   Background() : GameObject(IMGID_BACKGROUND, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, LAYER_BACKGROUND, WINDOW_WIDTH, WINDOW_HEIGHT, ANIMID_NO_ANIMATION) {}
   void OnClick() {}
   void Update() {}
+  int isZombie()
+  {
+    return -1;
+  }
+};
+
+class Sun : public GameObject
+{
+public:
+  Sun(int x, int y, bool z) : GameObject(IMGID_SUN, x, y, LAYER_SUN, 80, 80, ANIMID_IDLE_ANIM), m_isfromsky(z)
+  {
+    if (z)
+    {
+      Random random;
+      m_falltime = random.generateRandomNumber();
+    }
+    else
+    {
+      m_falltime = 12;
+    }
+  }
+  void OnClick()
+  {
+    SetHp(-2500);
+  }
+  void Update()
+  {
+    if (m_falltime > 0)
+    {
+      if (m_isfromsky)
+      {
+        MoveTo(GetX(), GetY() - 2);
+      }
+      else
+      {
+        MoveTo(GetX() - 1, GetY() + 4 - (12 - m_falltime));
+      }
+    }
+    if (m_falltime < -300)
+    {
+      SetHp(0);
+    }
+  }
+  int isZombie()
+  {
+    return -1;
+  }
+
+private:
+  int m_falltime;
+  bool m_isfromsky;
+};
+
+class PlantingSpot : public GameObject
+{
+public:
+  PlantingSpot(int x, int y) : GameObject(IMGID_WALLNUT, x, y, LAYER_UI, 60, 80, ANIMID_NO_ANIMATION) {}
+  void OnClick() {}
+  void Update() {}
+  int isZombie()
+  {
+    return -1;
+  }
 };
 #endif // !GAMEOBJECT_HPP__
--- src/GameWorld/GameWorld.cpp
+++ src/GameWorld/GameWorld.cpp
@@ -11,7 +11,13 @@ void GameWorld::Init()
   SetWave(0);
   SetSun(50);
   // Create Planting Spot
-
+  for (size_t i = 0; i != 5; i++)
+  {
+    for (size_t j = 0; j != 9; j++)
+    {
+      addobject(std::make_shared<PlantingSpot>(FIRST_COL_CENTER + j * LAWN_GRID_WIDTH, FIRST_ROW_CENTER + i * LAWN_GRID_HEIGHT));
+    }
+  }
   // Create Background
   addobject(std::make_shared<Background>());
 }
